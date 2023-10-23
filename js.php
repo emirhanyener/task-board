dd2fa741a6cf05d16b40642a904a78778fdc865c
emirhanyener <emirhan-yener@hotmail.com>
2023-10-23 17:43:22 +0300

    js: add config and random background

--- js.php
+++ js.php
@@ -1,4 +1,19 @@
 <script>
+    const config = {
+        "backgroundColor": "rgb(210, 255, 255)",
+        "randomColor": true
+    };
+
+    function randInt(minValue, maxValue) {
+        return (minValue + Math.floor(Math.random() * (maxValue - minValue)));
+    }
+    let minRGB = 220;
+    if(config.randomColor){
+        config.backgroundColor = "rgb(" + randInt(minRGB, 255) + ", " + randInt(minRGB, 255) + ", " + randInt(minRGB, 255) + ")"; 
+    }
+
+    document.querySelector("body").style.backgroundColor = config.backgroundColor;
+
     document.querySelector(".edit-task-panel-button").addEventListener("click", (event) => {
         document.querySelector(".edit-task-panel-back").style.visibility = "hidden";
     });
