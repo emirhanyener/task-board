da8ff18eff6c8ab8216f77915b1ce25427727442
emirhanyener <emirhan-yener@hotmail.com>
2023-10-23 17:05:27 +0300

    update js

--- js.php
+++ js.php
@@ -1,5 +1,12 @@
 <script>
-    for (let index = 0; index < document.getElementsByClassName("task-panel").length; index++) {
-        document.getElementsByClassName("task-panel")[index].innerHTML += '<form action="/?level=' + (index + 1) +'" method="post"><input type="text" class="add-task-text" name="taskvalue" placeholder="<?php echo $lang[$langcode]["task_value"] ?>"></form>';
+    document.querySelector(".edit-task-panel-button").addEventListener("click", (event) => {
+        document.querySelector(".edit-task-panel-back").style.visibility = "hidden";
+    });
+
+    function openEditPanel(filename, value){
+        document.querySelector(".edit-task-panel-back").style.visibility = "visible";
+        
+        document.querySelector(".edit-task-textarea").innerHTML = value;
+        document.querySelector("#edit-task-form").action = "?edit_task=" + filename;
     }
 </script>
