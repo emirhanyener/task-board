2ca71b756063e79faed6a35a7ecbf31566daf090
emirhanyener <emirhan-yener@hotmail.com>
2023-11-03 21:40:53 +0300

    add db.php

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+//////////////////////////
+//Start database connect//
+//////////////////////////
+    $host = "localhost";
+    $dbname = "task_board";
+    $username = "root";
+    $password = "";
+
+    try {
+        $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Database connection error: " . $e->getMessage());
+    }
+////////////////////////
+//End database connect//
+////////////////////////
+?>
