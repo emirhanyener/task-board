e9abc1da8113dc8f2b7233b866a98464a3a6455f
emirhanyener <emirhan-yener@hotmail.com>
2023-11-04 10:34:26 +0300

    index: add task user

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-//Config
-$langcode = "en";
+ob_start();
+session_start();
+include "db.php";
+include "config.php";
 
-$host = "localhost";
-$dbname = "task_board";
-$username = "root";
-$password = "";
+if(!isset($_SESSION["username"])){
+    header("Location: login.php");
+}
 
 //Localization
 $lang = array(
@@ -16,7 +17,9 @@ $lang = array(
         "task_value" => "Task Value",
         "edit_task" => "Edit Task",
         "edit" => "Edit",
-        "cancel" => "Cancel"
+        "cancel" => "Cancel",
+        "settings" => "Settings",
+        "logout" => "Logout"
     ),
     "tr" => array(
         "todo" => "Yapılacaklar",
@@ -25,25 +28,11 @@ $lang = array(
         "task_value" => "Görev içeriği",
         "edit_task" => "Görevi Düzenle",
         "edit" => "Düzenle",
-        "cancel" => "İptal Et"
+        "cancel" => "İptal Et",
+        "settings" => "Ayarlar",
+        "logout" => "Çıkış yap"
     )
 );
-
-ob_start();
-session_start();
-
-//////////////////////////
-//Start database connect//
-//////////////////////////
-try {
-    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Database connection error: " . $e->getMessage());
-}
-////////////////////////
-//End database connect//
-////////////////////////
 
 /////////////////
 //Start add log//
@@ -52,7 +41,8 @@ function addLog($db, $message) {
     date_default_timezone_set("Europe/Istanbul");
     $query = $db->prepare("INSERT INTO logs (timestamp, message) VALUES (:timestamp, :message)");
     $query->bindParam(":timestamp", date("[d.m.Y H:i:s]", time()));
-    $query->bindParam(":message", $message);
+    $new_message = "[".$_SESSION["username"]."]".htmlspecialchars($message);
+    $query->bindParam(":message", $new_message);
     $query->execute();
 }
 ///////////////
@@ -70,9 +60,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $query = $db->prepare("UPDATE tasks SET color = :color, value = :value WHERE id = :id");
         $query->bindParam(":color", $color);
-        $query->bindParam(":value", $value);
+        $query->bindParam(":value", htmlspecialchars($value));
         $query->bindParam(":id", $taskId);
         $query->execute();
+        addLog($db, "Edit Task(" .$taskId."): ". htmlspecialchars($value));
 
         header("Location: index.php");
         /////////////////
@@ -86,14 +77,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $taskcolor = $_POST["taskcolor"];
         $taskvalue = $_POST["taskvalue"];
 
-        $query = $db->prepare("INSERT INTO tasks (level, color, value) VALUES (:level, :color, :value)");
+        $query = $db->prepare("INSERT INTO tasks (level, color, value, userid) VALUES (:level, :color, :value, :userid)");
         $query->bindParam(":level", $level);
         $query->bindParam(":color", $taskcolor);
-        $query->bindParam(":value", $taskvalue);
+        $query->bindParam(":value", htmlspecialchars($taskvalue));
+        $query->bindParam(":userid", $_SESSION["userid"]);
         $query->execute();
 
         $lastInsertedId = $db->lastInsertId();
-        addLog($db, "add task: " . $lastInsertedId);
+        addLog($db, "Add Task(" .$lastInsertedId."): ". htmlspecialchars($taskvalue));
 
         header("Location: index.php");
         ////////////////
@@ -108,11 +100,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 if (isset($_GET["task_delete"])) {
     $taskId = $_GET["task_delete"];
     
-    $query = $db->prepare("DELETE FROM tasks WHERE id = :id");
-    $query->bindParam(":id", $taskId);
-    $query->execute();
+    $query = $db->query("SELECT * FROM tasks where id=$taskId; DELETE FROM tasks WHERE id = $taskId")->fetch();
 
-    addLog($db, "delete task: " . $taskId);
+    addLog($db, "Delete Task(" . $taskId . "): " . $query["value"]);
 
     header("Location: index.php");
 }
@@ -126,11 +116,9 @@ if (isset($_GET["task_delete"])) {
 if (isset($_GET["task_move_right"])) {
     $taskId = $_GET["task_move_right"];
     
-    $query = $db->prepare("UPDATE tasks SET level = level + 1 WHERE id = :id");
-    $query->bindParam(":id", $taskId);
-    $query->execute();
+    $query = $db->query("SELECT * FROM tasks where id=$taskId; UPDATE tasks SET level = level + 1 WHERE id = $taskId")->fetch();
 
-    addLog($db, "move right task: " . $taskId);
+    addLog($db, "Move Right Task(" . $taskId . "): " . $query["value"]);
 
     header("Location: index.php");
 }
@@ -144,11 +132,9 @@ if (isset($_GET["task_move_right"])) {
 if (isset($_GET["task_move_left"])) {
     $taskId = $_GET["task_move_left"];
     
-    $query = $db->prepare("UPDATE tasks SET level = level - 1 WHERE id = :id");
-    $query->bindParam(":id", $taskId);
-    $query->execute();
+    $query = $db->query("SELECT * FROM tasks where id=$taskId; UPDATE tasks SET level = level - 1 WHERE id = $taskId")->fetch();
 
-    addLog($db, "move left task: " . $taskId);
+    addLog($db, "Move Left Task(" . $taskId . "): " . $query["value"]);
 
     header("Location: index.php");
 }
@@ -196,7 +182,7 @@ if (isset($_GET["task_move_left"])) {
             <div class="task-item" oncontextmenu="CopyToClipboardFunction(event, '<?php echo $task['value']; ?>')">
                 <div style="background-color: <?php echo $task['color']; ?>;" class="task-item-color"></div>
                 <a href="?task_delete=<?php echo $task['id']; ?>" class="remove_task_link">X</a>
-                <div ondblclick="openEditPanel('<?php echo $task['id']; ?>', '<?php echo $task['value']; ?>', '<?php echo $task['color']; ?>')"><?php echo $task['value']; ?></div>
+                <div ondblclick="openEditPanel('<?php echo $task['id']; ?>', '<?php echo $task['value']; ?>', '<?php echo $task['color']; ?>')"><?php echo $task['value']; ?><br><font class="task-user-text"><?php echo $db->query("select * from users where id=".$task["userid"])->fetch()["username"]; ?></font></div>
                 <div class="task_move_arrows">
                     <a href="?task_move_right=<?php echo $task['id']; ?>" class="move_right_link">►</a>
                 </div>
@@ -216,7 +202,7 @@ if (isset($_GET["task_move_left"])) {
             <div class="task-item" oncontextmenu="CopyToClipboardFunction(event, '<?php echo $task['value']; ?>')">
                 <div style="background-color: <?php echo $task['color']; ?>;" class="task-item-color"></div>
                 <a href="?task_delete=<?php echo $task['id']; ?>" class="remove_task_link">X</a>
-                <div ondblclick="openEditPanel('<?php echo $task['id']; ?>', '<?php echo $task['value']; ?>', '<?php echo $task['color']; ?>')"><?php echo $task['value']; ?></div>
+                <div ondblclick="openEditPanel('<?php echo $task['id']; ?>', '<?php echo $task['value']; ?>', '<?php echo $task['color']; ?>')"><?php echo $task['value']; ?><br><font class="task-user-text"><?php echo $db->query("select * from users where id=".$task["userid"])->fetch()["username"]; ?></font></div>
                 <div class="task_move_arrows">
                     <a href="?task_move_left=<?php echo $task['id']; ?>" class="move_left_link">◄</a> 
                     <a href="?task_move_right=<?php echo $task['id']; ?>" class="move_right_link">►</a>
@@ -237,7 +223,7 @@ if (isset($_GET["task_move_left"])) {
             <div class="task-item" oncontextmenu="CopyToClipboardFunction(event, '<?php echo $task['value']; ?>')">
                 <div style="background-color: <?php echo $task['color']; ?>;" class="task-item-color"></div>
                 <a href="?task_delete=<?php echo $task['id']; ?>" class="remove_task_link">X</a>
-                <div ondblclick="openEditPanel('<?php echo $task['id']; ?>', '<?php echo $task['value']; ?>', '<?php echo $task['color']; ?>')"><?php echo $task['value']; ?></div>
+                <div ondblclick="openEditPanel('<?php echo $task['id']; ?>', '<?php echo $task['value']; ?>', '<?php echo $task['color']; ?>')"><?php echo $task['value']; ?><br><font class="task-user-text"><?php echo $db->query("select * from users where id=".$task["userid"])->fetch()["username"]; ?></font></div>
                 <div class="task_move_arrows">
                     <a href="?task_move_left=<?php echo $task['id']; ?>" class="move_left_link">◄</a> 
                 </div>
@@ -249,6 +235,9 @@ if (isset($_GET["task_move_left"])) {
             <input type="text" class="add-task-text" name="taskvalue" placeholder="<?php echo $lang[$langcode]["task_value"] ?>">
         </form>
         </div>
+    </div>
+    <div class="user-data-div">
+        <?php echo $_SESSION["username"]; ?> • <a href="settings.php"><?php echo $lang[$langcode]["settings"] ?></a> • <a href="logout.php"><?php echo $lang[$langcode]["logout"] ?></a>
     </div>
 </body>
 </html>
